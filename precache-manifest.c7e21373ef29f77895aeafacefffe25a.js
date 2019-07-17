190f642154fe4b35514b363b616a63d94811c777
Grzegorz Mazurek <mazurek.grz@gmail.com>
2019-07-17 11:44:17 +0200

    Updates

---
+++ precache-manifest.c7e21373ef29f77895aeafacefffe25a.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c48042b283d3c12aac025d3e322dc40d",
+    "url": "/daftplayer/index.html"
+  },
+  {
+    "revision": "003ddeda6cce5b5294b5",
+    "url": "/daftplayer/static/css/2.62bb572b.chunk.css"
+  },
+  {
+    "revision": "772d503db35980742c3e",
+    "url": "/daftplayer/static/css/main.157141d8.chunk.css"
+  },
+  {
+    "revision": "003ddeda6cce5b5294b5",
+    "url": "/daftplayer/static/js/2.49781ed9.chunk.js"
+  },
+  {
+    "revision": "772d503db35980742c3e",
+    "url": "/daftplayer/static/js/main.58c893d4.chunk.js"
+  },
+  {
+    "revision": "c5fa7710c455716ff37e",
+    "url": "/daftplayer/static/js/runtime~main.27ee17f3.js"
+  },
+  {
+    "revision": "d522478d9c40ce1d72360effa1c66521",
+    "url": "/daftplayer/static/media/Play_active.d522478d.png"
+  },
+  {
+    "revision": "1f83a8fdc99fd8420c8c3a8e4d36da39",
+    "url": "/daftplayer/static/media/SourceSansPro-Bold.1f83a8fd.woff2"
+  },
+  {
+    "revision": "ffffa8b47e700a4a75d856f3502afd54",
+    "url": "/daftplayer/static/media/SourceSansPro-Regular.ffffa8b4.woff2"
+  },
+  {
+    "revision": "0d7e4b7688d4215d48f8e3d994172119",
+    "url": "/daftplayer/static/media/back_ico.0d7e4b76.svg"
+  },
+  {
+    "revision": "52d51b7d5b834b033266df7aeaa497b9",
+    "url": "/daftplayer/static/media/bg_image.52d51b7d.jpg"
+  },
+  {
+    "revision": "cb52536c1e2964c14f9b7f0b930a6b23",
+    "url": "/daftplayer/static/media/daft_logo.cb52536c.svg"
+  },
+  {
+    "revision": "8789af58250d446892d7a6b79f6ee0ae",
+    "url": "/daftplayer/static/media/hide_ico.8789af58.svg"
+  },
+  {
+    "revision": "b419075f1382bf3c214ede93fcee91bc",
+    "url": "/daftplayer/static/media/more_ico.b419075f.svg"
+  },
+  {
+    "revision": "c0ec82d4e5d8afc6aa43e29008fb5988",
+    "url": "/daftplayer/static/media/next_ico.c0ec82d4.svg"
+  },
+  {
+    "revision": "417cc67ce7dff71de20950bfaac897a2",
+    "url": "/daftplayer/static/media/playlist_ico.417cc67c.svg"
+  },
+  {
+    "revision": "078ae9df75229ee1048ca1eeac7962b7",
+    "url": "/daftplayer/static/media/previous_ico.078ae9df.svg"
+  },
+  {
+    "revision": "1057333e78a18b075330c5585f7c1a71",
+    "url": "/daftplayer/static/media/repeat_ico.1057333e.svg"
+  },
+  {
+    "revision": "0dd78418e2ebf2663184ee06efe8ed47",
+    "url": "/daftplayer/static/media/shuffle_ico.0dd78418.svg"
+  }
+]);
